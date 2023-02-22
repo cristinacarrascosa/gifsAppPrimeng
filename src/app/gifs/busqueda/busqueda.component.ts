8b645fd99570d5086d42294deebe7d3b2acbf011
cristinacarrascosa <cctausias@gmail.com>
2023-02-22 19:13:15 +0100

    ejemplo en input de busqueda con viewChild

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import {InputTextModule} from 'primeng/inputtext';
 
 @Component({
@@ -7,5 +7,16 @@ import {InputTextModule} from 'primeng/inputtext';
   styleUrls: ['./busqueda.component.scss']
 })
 export class BusquedaComponent {
+
+// viewChild es una propiedad que nos permite acceder a un elemento del DOM
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>; //ElementRef es de tipo any por defecto, le indicamos que es de tipo HTMLInputElement
+
+
+  buscar() {
+    const valor = this.txtBuscar.nativeElement.value;
+    console.log(valor);
+    // Limpiar el input
+    this.txtBuscar.nativeElement.value = '';
+  }
 
 }
